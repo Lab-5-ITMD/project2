034e477024b56123952884c17842027c5a97c8c1
zansari18 <143058076+zansari18@users.noreply.github.com>
2024-04-18 17:08:25 -0500

    Moving around css
    I added my js, and am currently moving all the CSS to one file

--- js/welcome.js
+++ js/welcome.js
@@ -1,4 +1,24 @@
-// jQuery 3.x-style ready event and locally scoped $
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+const drop = document.getElementById("dropbtn");
+var i;
+
+for (i = 0; i < drop.length; i++) {
+  drop[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
