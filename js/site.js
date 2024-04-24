e9ce0332bcae4fe8d4963693b38cb695c86f8634
Ella Brady <112596525+ellaroseb@users.noreply.github.com>
2024-04-24 16:39:13 -0500

    email validation js added

--- js/site.js
+++ js/site.js
@@ -1,4 +1,8 @@
-
+function ValidateEmail(email) {
+  var re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
